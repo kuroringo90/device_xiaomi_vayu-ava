2b9e1ac64a5bb5610b1bdd4de9ca03d283ec0537
Yifan Hong <elsk@google.com>
2018-08-10 13:04:20 -0700

    vayu: Add health.storage@1.0 HAL implementation
    Test: lshal debug android.hardware.health.storage@1.0::IStorage/default
    Test: run `adb shell sm idle-maint run`
    
    Bug: 116500049
    
    Change-Id: Ib9ffb1a0926f49fee557c27a5d542aad90682006
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- device.mk
+++ device.mk
@@ -250,6 +250,10 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service
 
+# Storage health HAL
+PRODUCT_PACKAGES += \
+    android.hardware.health.storage@1.0-service
+
 # Google Camera Go
 PRODUCT_PACKAGES += \
     GoogleCameraGo
