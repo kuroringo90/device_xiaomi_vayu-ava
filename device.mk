6db4ab2d844dc98ac730210b21d679d6b45a5d27
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-11-04 15:04:40 +0500

    vayu: Use Pixel offline charging animation
    * from redfin-user RD1A.201105.003.A1-6886512
    
    Change-Id: I25e5b0e8cb6e463fb273227482461692ab8b6272

--- device.mk
+++ device.mk
@@ -387,6 +387,11 @@ PRODUCT_PACKAGES += \
    TetheringConfigOverlayVayu \
    WifiOverlayVayu
 
+# Pixel Offline charger
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    product_charger_res_images
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-sm8150-libperfmgr \
