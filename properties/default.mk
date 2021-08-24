23080951f486a34c87933984f2fff8c68e909ee2
Unicote <arvogl874@gmail.com>
2021-08-24 23:37:10 +0300

    vayu: props: Drop ro.surface_flinger.force_hwc_copy_for_virtual_displays
    * Fixes lags with PiP
    
    Signed-off-by: Unicote <arvogl874@gmail.com>
    Signed-off-by: alanndz <alanndz@qmail.id>

--- properties/default.mk
+++ properties/default.mk
@@ -16,7 +16,6 @@
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     debug.sf.frame_rate_multiple_threshold=120 \
-    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
     ro.surface_flinger.has_HDR_display=true \
     ro.surface_flinger.has_wide_color_display=false \
