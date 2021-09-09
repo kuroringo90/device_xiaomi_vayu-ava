50c61406a6ecbaf126b4bfe2daa2b023d7fefb8b
Rafiester <rafi.haqqi@gmail.com>
2021-09-09 10:15:20 +0700

    vayu: Syberia-Project Bringup
    Signed-off-by: Rafiester <rafi.haqqi@gmail.com>

--- syberia_vayu.mk
+++ syberia_vayu.mk
@@ -11,10 +11,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
-# Inherit common ArrowOS configurations
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit common Syberia configurations
+$(call inherit-product, vendor/syberia/common.mk)
 
-PRODUCT_NAME := arrow_vayu
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# FaceUnlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# GApps
+TARGET_GAPPS_ARCH := arm64
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := syberia_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := Poco X3 Pro
@@ -26,6 +36,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="vayu_global-user 11 RKQ1.200826.002 V12.5.4.0.RJUMIXM release-keys"
 
 BUILD_FINGERPRINT := POCO/vayu_global/vayu:11/RKQ1.200826.002/V12.5.4.0.RJUMIXM:user/release-keys
-
-DEVICE_MAINTAINER := kubersharma001
-
