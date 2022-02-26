3e0244044c6f5c453b38cf96f9dad23636885385
alanndz <alanndz7@gmail.com>
2022-02-26 13:06:41 +0000

    vayu: Move to Pixel Thermal HAL 2.0
    This is also squashed commit of :
    vayu: sepolicy: Kang sepolicy rules for thermal HAL
    vayu: rootdir: Setup init for Thermal HAL
    vayu: Import libthermalclient from barbet

--- device.mk
+++ device.mk
@@ -559,6 +559,15 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_vayu/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_vayu/com.nxp.mifare.xml
 
+# Thermal HAL
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.pixel \
+    thermal_symlinks
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
+    $(LOCAL_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
+
 # Touchfeature
 PRODUCT_PACKAGES += \
     vendor.xiaomi.hardware.touchfeature@1.0.vendor
