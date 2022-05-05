5469c5c6763fb43b6718b23c03cd9aff927af2d8
johnmart19 <johnivan19999@gmail.com>
2022-05-06 02:09:56 +0300

    vayu: Add compilation of legacy Audio for muting logspam
    - We are recieving errors on Init about this, so let's satisfy environment:
    [    7.888683] init: Command 'restart vendor.audio-hal-4-0-msd' action=onrestart (<Service 'audioserver' onrestart>:2) took 0ms and failed: service vendor.audio-hal-4-0-msd not found
    [    7.888698] init: Command 'restart audio_proxy_service' action=onrestart (<Service 'audioserver' onrestart>:3) took 0ms and failed: service audio_proxy_service not found
    [    7.888722] init: Command 'restart vendor.audio-hal-2-0' action=onrestart (<Service 'audioserver' onrestart>:4) took 0ms and failed: service vendor.audio-hal-2-0 not found
    [    7.888729] init: Command 'restart audio-hal-2-0' action=onrestart (<Service 'audioserver' onrestart>:5) took 0ms and failed: service audio-hal-2-0 not found
    
    Signed-off-by: Rafiester <rafi.haqqi@gmail.com>

--- device.mk
+++ device.mk
@@ -79,6 +79,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
+# Audio-Legacy for Muting logspam
+PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio@4.0-impl
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.1-impl \
