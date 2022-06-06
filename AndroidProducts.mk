855f6c3469c294a68d8874e970f0c6bb2ac3404e
kuroringo90 <ssss9999@gmx.it>
2022-06-06 22:27:40 +0200

    Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/superior_vayu.mk
+    $(LOCAL_DIR)/proton_vayu.mk
 
 COMMON_LUNCH_CHOICES := \
-    superior_vayu-eng \
-    superior_vayu-userdebug \
-    superior_vayu-user
+    proton_vayu-eng \
+    proton_vayu-userdebug \
+    proton_vayu-user
