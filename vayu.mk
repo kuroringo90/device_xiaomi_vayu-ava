40642f1f7f20aadb6e8e3904e299acbee04f2d56
ste0090 <ste0090@AzureServer.ahfm5tgevvze5or4moti23gbgd.px.internal.cloudapp.net>
2022-06-07 15:44:17 +0200

    Zephyrus: Updated bringup and broken policy

---
+++ vayu.mk
@@ -0,0 +1,45 @@
+#
+# Copyright (C) 2018-2021 ArrowOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit common products
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit device configurations
+$(call inherit-product, device/xiaomi/vayu/device.mk)
+
+# QuickTap
+TARGET_SUPPORTS_QUICK_TAP := true
+
+# Pixel Charger
+USE_PIXEL_CHARGER_IMAGES := true
+
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# FaceUnlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# GApps
+WITH_GMS := true
+
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := vayu
+PRODUCT_DEVICE := vayu
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := Poco X3 Pro
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="vayu_id-user 12 SKQ1.211006.001 V13.0.2.0.SJUIDXM release-keys" \
+    PRODUCT_NAME=vayu_global \
+    PRODUCT_MODEL=M2102J20SI
+
+BUILD_FINGERPRINT := POCO/vayu_id/vayu:12/SKQ1.211006.001/V13.0.2.0.SJUIDXM:user/release-keys
