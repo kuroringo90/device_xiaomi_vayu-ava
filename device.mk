45ab7721cf8747b7a977f8d2dc014e5f76726931
Simone <60412798+Nesquirt@users.noreply.github.com>
2022-06-09 02:52:48 +0200

    Remove vendor.qti.hardware.display.allocator-service

--- device.mk
+++ device.mk
@@ -196,7 +196,6 @@ PRODUCT_PACKAGES += \
     vendor.display.config@1.11.vendor \
     vendor.display.config@2.0 \
     vendor.display.config@2.0.vendor \
-    vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.mapper@1.1.vendor \
     vendor.qti.hardware.display.mapper@2.0.vendor \
     vendor.qti.hardware.display.mapper@3.0.vendor \
